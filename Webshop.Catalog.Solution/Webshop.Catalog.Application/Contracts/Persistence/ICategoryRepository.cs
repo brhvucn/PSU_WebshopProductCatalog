8db42db65467d75a52468b12a11d02e64c8b54b8
Brian Hvarregaard <brian@median.dk>
2022-06-17 15:10:35 +0200

    Building domain entities for catalog

---
+++ Webshop.Catalog.Solution/Webshop.Catalog.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Catalog.Application.Contracts.Persistence
+{
+    public class ICategoryRepository
+    {
+    }
+}
---
+++ Webshop.Catalog.Solution/Webshop.Catalog.Domain/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webshop.Domain.Common;
+
+namespace Webshop.Catalog.Domain.Entities
+{
+    public class Category : Entity
+    {
+        public Category(string name)
+        {
+            Name = name;
+            ChildCategories = new List<Category>();
+        }
+        public string Name { get; private set; }
+        public string Description { get; set; }
+        public List<Category> ChildCategories { get; set; }
+    }
+}
