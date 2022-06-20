41847cc9fb7e7ee0fc04f3a2454ffff881d031b2
Brian Hvarregaard <brian@median.dk>
2022-06-20 20:00:56 +0200

    database tables

---
+++ Webshop.Catalog.Solution/Product.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Product](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](150) NOT NULL,
+	[SKU] [nvarchar](50) NOT NULL,
+	[Price] [int] NOT NULL,
+	[Currency] [nvarchar](3) NOT NULL,
+	[Description] [ntext] NULL,
+	[AmountInStock] [int] NULL,
+	[MinStock] [int] NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)
+)
+
---
+++ Webshop.Catalog.Solution/ProductCategory.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ProductCategory](
+	[ProductId] [int] NOT NULL,
+	[CategoryId] [int] NOT NULL,
+ CONSTRAINT [PK_ProductCategory] PRIMARY KEY CLUSTERED 
+(
+	[ProductId] ASC,
+	[CategoryId] ASC
+)
+)
+
