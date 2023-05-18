fd6644905cb776fe5c789ee13049bc991b567a66
BranikMacik <branislavmacko2@gmail.com>
2023-05-18 18:54:38 +0200

    Adds classes to the Order.Application project

---
+++ Webshop.Catalog.Solution/Webshop.Order.Application/OrderApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Webshop.Application;
+using Webshop.Application.Contracts;
+
+namespace Webshop.Order.Application
+{
+    public static class OrderApplicationServiceRegistration
+    {
+        public static IServiceCollection AddCustomerApplicationServices(this IServiceCollection services)
+        {
+            services.AddApplicationServices();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddScoped<IDispatcher>(sp => new Dispatcher(sp.GetService<IMediator>()));
+
+            return services;
+        }
+    }
+}
