96cb5bb2ec7b43a9336cbd70ce248be843efcfbe
BranikMacik <branislavmacko2@gmail.com>
2023-05-18 19:00:35 +0200

    Adds general structure to the Order project

---
+++ Webshop.Catalog.Solution/Webshop.Order.Application/Features/Category/Dtos/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Order.Application.Features.Category.Dtos
+{
+    internal interface OrderDto
+    {
+    }
+}
