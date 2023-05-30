d0f740acee40bd751d980b6bca803a96d2956f65
BranikMacik <branislavmacko2@gmail.com>
2023-05-30 14:17:12 +0200

    Adds functionality to OrderDto and GetOrderQuery

--- Webshop.Catalog.Solution/Webshop.Order.Application/Features/Order/Dtos/OrderDto.cs
+++ Webshop.Catalog.Solution/Webshop.Order.Application/Features/Order/Dtos/OrderDto.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Webshop.Domain.AggregateRoots;
 
 namespace Webshop.Order.Application.Features.Category.Dtos
 {
     public class OrderDto
     {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Discount { get; set; }
+        public Dictionary<Catalog.Domain.AggregateRoots.Product, int> OrderedProducts { get; set; }
     }
 }
--- Webshop.Catalog.Solution/Webshop.Order.Application/Features/Order/Queries/GetOrders/GetOrderQuery.cs
+++ Webshop.Catalog.Solution/Webshop.Order.Application/Features/Order/Queries/GetOrders/GetOrderQuery.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Webshop.Application.Contracts;
+using Webshop.Order.Application.Features.Category.Dtos;
 
 namespace Webshop.Order.Application.Features.Category.Queries.GetOrders
 {
-    public class GetOrderQuery
+    public class GetOrderQuery : IQuery<OrderDto>
     {
+        public GetOrderQuery(int orderId)
+        {
+            OrderId  = orderId;
+        }
+
+        public int OrderId { get; set; }
     }
 }
