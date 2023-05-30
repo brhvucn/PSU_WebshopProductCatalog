d8c80b538a43370081e108583f500929019fa5ba
BranikMacik <branislavmacko2@gmail.com>
2023-05-30 20:24:36 +0200

    Adds Handler classes

---
+++ Webshop.Catalog.Solution/Webshop.Order.Application/Features/Order/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webshop.Application.Contracts;
+using Webshop.Domain.Common;
+using Webshop.Order.Application.Contracts.Persistance;
+
+namespace Webshop.Order.Application.Features.Order.Commands.CreateOrder
+{
+    internal class CreateOrderCommandHandler : ICommandHandler<CreateOrderCommand>
+    {
+        private ILogger<CreateOrderCommandHandler> logger;
+        private IOrderRepository orderRepository;
+
+        public CreateOrderCommandHandler(ILogger<CreateOrderCommandHandler> logger, IOrderRepository orderRepository)
+        {
+            this.logger = logger;
+            this.orderRepository = orderRepository;
+        }
+
+        public Task<Result> Handle(CreateOrderCommand command, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
