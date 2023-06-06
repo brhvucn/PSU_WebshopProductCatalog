e64d43843c4a1006fb4b647372c7b51c9dc08e7b
BranikMacik <branislavmacko2@gmail.com>
2023-06-06 11:36:40 +0200

    Added the name of a table in the TableName.cs file

--- Webshop.Catalog.Solution/Webshop.Data.Persistence/TableNames.cs
+++ Webshop.Catalog.Solution/Webshop.Data.Persistence/TableNames.cs
@@ -18,6 +18,11 @@ namespace Webshop.Data.Persistence
         public static class Customer
         {
             public static string CUSTOMERTABLE = "Customer";
-        }                
+        }               
+        
+        public static class Order
+        {
+            public static string ORDERTABLE = "Order";
+        }
     }
 }
---
+++ Webshop.Catalog.Solution/Webshop.Order.Persistance/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webshop.Application.Contracts.Persistence;
+
+namespace Webshop.Order.Persistance
+{
+    public interface IOrderRepository : IRepository<Domain.AggregateRoots.Order>
+    {
+    }
+}
