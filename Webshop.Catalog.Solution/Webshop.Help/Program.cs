a6e44d462debc84b869758d3a95d28125a89277b
Brian Hvarregaard <brian@median.dk>
2024-07-02 16:21:32 +0200

    more logging added and new port adresses

--- Webshop.Catalog.Solution/Webshop.Help/Program.cs
+++ Webshop.Catalog.Solution/Webshop.Help/Program.cs
@@ -1,7 +1,24 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+//use serilog
+var configuration = builder.Configuration;
+string seqUrl = configuration.GetValue<string>("Settings:SeqLogAddress");
+// Serilog configuration
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Service", "Webshop.Help") // Enrich with the tag "Service" and the name of this service
+    .WriteTo.Seq(seqUrl)
+    .CreateLogger();
+//add serilog
+//add the logger
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
 
 var app = builder.Build();
 
