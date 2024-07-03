a32a017fe362376f507fcbdce87e9576d43833b8
Brian Hvarregaard <brian@median.dk>
2024-07-03 13:11:00 +0200

    Added support for request aggregation

--- Webshop.Catalog.Solution/Webshop.ReviewServiceGateway/Program.cs
+++ Webshop.Catalog.Solution/Webshop.ReviewServiceGateway/Program.cs
@@ -4,8 +4,9 @@ using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 //add Ocelot Config
-builder.Configuration.AddJsonFile("ocelot.json");
+//builder.Configuration.AddJsonFile("ocelot.json");
 //builder.Configuration.AddJsonFile("ocelotloadbalancer.json");
+builder.Configuration.AddJsonFile("ocelotrequestaggregator.json");
 //add Ocelot
 builder.Services.AddOcelot(builder.Configuration);
 
